dc8c672c641c79d7b8c345f6fc1499e4db26ea0d
PietroFiori91 <pederflowers@hotmail.it>
2023-07-03 18:16:23 +0200

    progress su biglietto generato

--- js/main.js
+++ js/main.js
@@ -40,21 +40,43 @@
 // _______________________________________________________
 
 
-const usernameInputElement = document.querySelector("[name='username']")
-const kmpercorsiInputElement = document.querySelector("[name='kmpercorsi']")
-const btnGenera = document.querySelector(".btn-genera")
+const usernameInputElement = document.querySelector("[name='username']");
+const kmInputElement = document.querySelector("[name='kmpercorsi']");
+const etàInputElement = document.querySelector("[name='etàpasseggero']");
 
+const btnGenera = document.querySelector(".btn-genera");
 
 btnGenera.addEventListener("click", function() {
-    console.log("click")
-//     if (userAge < 18) {
-//         const sconto20 = (prezzoBiglietto * 20) / 100;
-//         console.log("L'utente ha diritto a" + " " + sconto20 + " " + "Euro" + " " + "di sconto");
-//     } else if (userAge >= 18 && userAge < 65) {
-//         const noSconto = prezzoBiglietto;
-//         console.log("L'utente ha diritto a" + " " + 0 + " " + "Euro" + " " + "di sconto");
-//     } else {
-//         const sconto40 = (prezzoBiglietto * 40) / 100;
-//         console.log("L'utente ha diritto a" + " " + sconto40 + " " + "Euro" + " " + "di sconto");
-//     }
-})
+    console.log("click");
+
+    const username = usernameInputElement.value;
+    const km = parseInt(kmInputElement.value);
+    const età = parseInt(etàInputElement.value);
+
+    const prezzoBiglietto = (km * 0.21).toFixed(2);
+    const sconto20 = (prezzoBiglietto * 20) / 100;
+    const noSconto = prezzoBiglietto;
+    const sconto40 = (prezzoBiglietto * 40) / 100;
+
+    let sconto=0
+
+    if (età < 18) {
+        sconto = (prezzoBiglietto * 20) / 100;
+        console.log("L'utente ha diritto a " + sconto.toFixed(2) + " Euro di sconto");
+        document.getElementById("username-biglietto").innerHTML = username
+        document.getElementById("costo-finale-biglietto").innerHTML = sconto20
+    } else if (età >= 18 && età < 65) {
+        sconto = 0;
+        console.log("L'utente non ha diritto ad alcuno sconto");
+        document.getElementById("username-biglietto").innerHTML = username
+        document.getElementById("costo-finale-biglietto").innerHTML = noSconto
+    } else {
+        sconto = (prezzoBiglietto * 40) / 100;
+        console.log("L'utente ha diritto a " + sconto.toFixed(2) + " Euro di sconto");
+        document.getElementById("username-biglietto").innerHTML = username
+        document.getElementById("costo-finale-biglietto").innerHTML = sconto40
+    }
+
+    const finalPrice = (prezzoBiglietto - sconto).toFixed(2);
+    console.log("Prezzo del biglietto: " + finalPrice + " Euro");
+});
